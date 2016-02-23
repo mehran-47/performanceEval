8c32da71f156ad484bee72c0b224c198d62fbd13
mehran <i@mehrankhan.com>
2016-02-23 17:17:21 -0500

    turned to class

--- evaluate.py
+++ evaluate.py
@@ -4,27 +4,34 @@ from subprocess import Popen, PIPE
 from pexpect import spawn
 from threading import Thread
 
-def byName(names):
-	logging.debug(names)
-	allProcsOfName = [p for p in map(lambda n: ps.Process(n), ps.pids()) if p.name() in names]
-	#logging.debug(allProcsOfName)
-	#logging.debug(len(allProcsOfName))
-	return listByPsProcs(allProcsOfName)
+class evaluate():
+	def __init__(self):
+		pass
 
-def dictByName(names):
-	listByNames = byName(names)
-	return dict((name, {x for x in listByNames if name in x.name()}) for name in names)
+	def byName(self, names):
+		logging.debug(names)
+		allProcsOfName = [p for p in map(lambda n: ps.Process(n), ps.pids()) if p.name() in names]
+		#logging.debug(allProcsOfName)
+		#logging.debug(len(allProcsOfName))
+		return self.listByPsProcs(allProcsOfName)
 
-def dictByPid(pids):
-	procs = [ps.Process(int(p)) for p in pids]
-	return dict((p.pid,{p} | set(p.children(recursive=True))) for p in procs)
+	def dictByName(self, names):
+		listByNames = self.byName(names)
+		return dict((name, {x for x in listByNames if name in x.name()}) for name in names)
 
-def listByPsProcs(ps_procs):
-	listOfSets = [{p}|set(p.children(recursive=True)) for p in ps_procs]
-	return [proc for aSet in listOfSets for proc in aSet]
+	def dictByPid(self, pids):
+		procs = [ps.Process(int(p)) for p in pids]
+		return dict((p.pid,{p} | set(p.children(recursive=True))) for p in procs)
 
-def dictOfConnectionsByPid(ps_procs):
-	return dict((p.pid, [p.name(), p.connections()]) for p in ps_procs if p.connections()[0:])
+	def listByPsProcs(self, ps_procs):
+		listOfSets = [{p}|set(p.children(recursive=True)) for p in ps_procs]
+		return [proc for aSet in listOfSets for proc in aSet]
+
+	def dictOfConnectionsByPid(self, ps_procs):
+		return dict((p.pid, [p.name(), p.connections()]) for p in ps_procs if p.connections()[0:])
+
+	def updateWith(self):
+		pass
 
 def testDef():
 	child = spawn('bash')
@@ -39,14 +46,15 @@ def testDef():
 
 if __name__ == '__main__':
 	logging.basicConfig(format= '%(asctime)s %(levelname)s - %(name)s\n%(message)s', level=logging.DEBUG)
+	e = evaluate()
 	if sys.argv[1:]:
 		if sys.argv[1]=='-pid':
-			logging.info("%r,\n%d" %(listByPsProcs(map(ps.Process, map(int, sys.argv[2:]))), len(listByPsProcs(map(ps.Process, map(int, sys.argv[2:])) ) ) ) )
+			logging.info("%r,\n%d" %(e.listByPsProcs(map(ps.Process, map(int, sys.argv[2:]))), len(e.listByPsProcs(map(ps.Process, map(int, sys.argv[2:])) ) ) ) )
 		elif sys.argv[1]=='-name':
-			logging.info(dictByName(sys.argv[2:]))
-			logging.debug("%r" %(dict((k,len(v)) for k,v in dictByName(sys.argv[2:]).items() ) ) )
+			logging.info(e.dictByName(sys.argv[2:]))
+			logging.debug("%r" %(dict((k,len(v)) for k,v in e.dictByName(sys.argv[2:]).items() ) ) )
 		elif sys.argv[1]=='-connections':
-			logging.info(dictOfConnectionsByPid(byName(sys.argv[2:])))
+			logging.info(e.dictOfConnectionsByPid(e.byName(sys.argv[2:])))
 		else:
 			Thread(target=testDef).start()
 	else:
