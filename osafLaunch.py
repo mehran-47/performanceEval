233233047ea19cd1d7a50108ed986bbf5443d74e
mehran <i@mehrankhan.com>
2016-02-24 16:45:46 -0500

    added osafLaunch

---
+++ osafLaunch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import psutil as ps, sys
+from subprocess import call
+if __name__ == '__main__':
+	if sys.argv[1:]:
+		allOsafProcs = [p.name() for p in map(lambda n: ps.Process(n), ps.pids()) if 'osaf' in p.name()]
+		call(['./evaluate.py']+allOsafProcs+[sys.argv[-1]])
