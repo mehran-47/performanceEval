a7faa621c051e317cb98acadddad70f458cb3bdd
mehran <i@mehrankhan.com>
2016-02-24 16:54:51 -0500

    converted osafLaunch output to a string temporarily

--- osafLaunch.py
+++ osafLaunch.py
@@ -4,4 +4,6 @@ from subprocess import call
 if __name__ == '__main__':
 	if sys.argv[1:]:
 		allOsafProcs = [p.name() for p in map(lambda n: ps.Process(n), ps.pids()) if 'osaf' in p.name()]
-		call(['./evaluate.py']+allOsafProcs+[sys.argv[-1]])
+		l = ['./evaluate.py']+allOsafProcs+[sys.argv[-1]]
+		print(" ".join(l))
+		#call(['./evaluate.py']+allOsafProcs+[sys.argv[-1]])
